bdaf2b2cc243a4ab96593d51867884301bef5342
Alberto <jalberto.robles@outlook.com>
2018-06-14 20:56:53 -0700

    Web dev finished

---
+++ js/general.js
@@ -0,0 +1,6 @@
+$(document).ready(function (){
+    
+    var battleborn = "../../assets/BattleBorn.wav";
+    var nightsky = "../../assets/NightSky.wav";
+    var tragic = "../../assets/tragic.wav"
+});
