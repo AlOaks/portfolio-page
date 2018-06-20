47e069d7ffa2bae3b9253e54445cef9324ae80d8
Alberto <jalberto.robles@outlook.com>
2018-06-20 15:56:24 -0700

    Changes & Improvements

--- js/general.js
+++ js/general.js
@@ -10,7 +10,13 @@ $(document).ready(function (){
     $('.p1').click(function(){
         $('.p1').hide();
         $('.pa1').show();
+        $('.pa2').hide();
+        $('.p2').show();
+        $('.p3').show();
+        $('.pa3').hide();
         battleborn.play();
+        nightsky.pause();
+        tragic.pause();
     });
 
     $('.pa1').click(function(){
@@ -22,46 +28,87 @@ $(document).ready(function (){
     $('.p2').click(function(){
         $('.p2').hide();
         $('.pa2').show();
+        $('.pa1').hide();
+        $('.p1').show();
+        $('.p3').show();
+        $('.pa3').hide();
         nightsky.play();
+        battleborn.pause();
+        tragic.pause();
     });
 
     $('.pa2').click(function(){
         $('.pa2').hide();
         $('.p2').show();
         nightsky.pause();
+        
     });
 
     $('.p3').click(function(){
         $('.p3').hide();
         $('.pa3').show();
+        $('.pa1').hide();
+        $('.p1').show();
+        $('.pa2').hide();
+        $('.p2').show();
         tragic.play();
+        battleborn.pause();
+        nightsky.pause();
     });
 
     $('.pa3').click(function(){
         $('.pa3').hide();
         $('.p3').show();
         tragic.pause();
+        
     });
 
 
     // DESKTOP
 
+
+
     function checkSize() {
 
-        $('.web-circle').click(function(){
+        $('.fa-desktop').click(function(){
             $('.hiddenw').css('visibility', 'visible');
             $('.hidden').css('visibility', 'hidden');
 
         });
 
-        $('.music-circle').click(function(){
+        $('.fa-headphones').click(function(){
             $('.hidden').css('visibility', 'visible');
             $('.hiddenw').css('visibility', 'hidden');
+        
         });
         
     }
 
+    function hiding() {
+
+    }
+
+    hiding();
     checkSize();
-    
+
+    $('.fa-envelope').click(function(){
+        $('.email').css('font-size', '20px');
+    });
+
+    $('.fa-phone').click(function(){
+        $('.phone').css('font-size', '20px');
+    });
+
+    // function document() {
+
+    //     $(document).click(function(){
+    //         $('.hidden').css('visibility', 'hidden');
+    //         $('.hiddenw').css('visibility', 'hidden');
+    //     });
+    // }
+
+    // document();
+
+   
    
 });
